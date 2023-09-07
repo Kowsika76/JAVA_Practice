364f16cc43ee5db30a737445b4a81bfc9490422b
Kowsi <kowsikam123@gmail.com>
2023-09-07 18:11:35 +0530

    modified in merge array quest

--- MergeArrayList.java
+++ MergeArrayList.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 
 public class MergeArrayList {
     public static void main(String[]args){
-        ArrayList<String> names = new ArrayList<String>();
-        ArrayList<String> lastNames = new ArrayList<String>();
+        ArrayList<String> firstNames = new ArrayList<String>();
+        ArrayList<String> lastNames  = new ArrayList<String>();
        Scanner sc = new Scanner(System.in);
 
-       System.out.println("Enter the list length:");
-       int arrsize = sc.nextInt();
-        System.out.println("Enter "+arrsize+" first names:");
-        for(int i=0;i<arrsize;i++){
-            names.add(sc.nextLine()); 
-        }System.out.println("First names:"+names);
+      //System.out.println("Enter the list length:");
+       //int length = sc.nextInt();
+        int length = 5;
+        System.out.println("Enter first "+length+" names:");
+        for(int i=0;i<length;i++){
+            firstNames.add(sc.nextLine()); 
+        }System.out.println("First names:"+firstNames);
 
-        System.out.println("Enter "+arrsize+" second names:");
-        for(int j=0;j<arrsize;j++){
+
+        System.out.println("Enter second "+length+" names:");
+        for(int j=0;j<length;j++){
             lastNames.add(sc.nextLine()); 
         }System.out.println("second names:"+lastNames);
 
-        for(int i=0;i<arrsize;i++){
-                System.out.println("Your full name "+names.get(i)+" "+lastNames.get(i));
-            }
+        System.out.println("-------Your full name--------- ");
+        for(int i=0;i<length;i++){
+                String fullName= firstNames.get(i)+" "+lastNames.get(i);
+                System.out.println(fullName);
+            }sc.close();
         }
         }
